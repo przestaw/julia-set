ba5c99a1e3bc7eb3601eb296a49b5985962c0fb7
przestaw <przemek.stawczyk@gmail.com>
2018-11-27 15:52:25 +0100

    added changable size and padding support

--- MIPS/Julia.asm
+++ MIPS/Julia.asm
@@ -1,15 +1,130 @@
-.data
+.eqv BUFF 3000 #must be dividable by 3 !!!!
 
-image: .space 786432 # 512x512 8-bit image
+.data 
+.align 3
+image: .space BUFF
+output: .asciiz "julia-V9.bmp"
 real_q: .asciiz "\nNumber format 0.0000\nEnter the real part of constatnt\n0."
 imaginary_q: .asciiz "\nEnter the imaginary part of constatnt\n0."
-input: .asciiz "input.bmp"
-output: .asciiz "julia.bmp"
+size_q: .asciiz "Enter the size of image in pixels\n"
+msg_1: .asciiz "Generated BMP header and opening file...\n"
+comm_1: .asciiz "Size must be multiplication of 4 assumming:"
+msg_2: .asciiz "Done\n"
+.align 2
 header_bmp: .space 54
 
 	.text
 ## Start of code section
 main:
+	li $v0, 4           
+	la $a0, size_q
+	# ask for size
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $ra, $v0
+	# $ra - size of the image
+	# $fp - number of tot bytes
+	
+	li $t2, 4
+	divu $ra, $t2
+	mfhi $t9
+	#subu $t9, $t2, $t9
+bmp_header_gen:	#unused - for cleaner code
+## GEN BMP header	
+	la $t1, header_bmp
+	li $t2, 'B'
+	sb $t2, ($t1)
+	addiu $t1, $t1, 1
+	li $t2, 'M'
+	sb $t2, ($t1)
+	addiu $t1, $t1, 1
+	
+	#FILE_SIZE
+	mul $t2, $ra, 3
+	mul $fp, $t2, $ra #X*Y*3
+	
+	mul $t2, $ra, $t9
+	addu $fp, $fp, $t2 # blank spaces
+	
+	addiu $t3, $fp, 54
+	sh $t3, ($t1)
+	addiu $t1, $t1, 2
+	srl $t2, $t3, 16
+	sh $t2, ($t1)
+	addiu $t1, $t1, 2
+	#UNUSED	
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	#PIX_OFFSET
+	li $t2, 54
+	sh $t2, ($t1)
+	addiu $t1, $t1, 2
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	#DIB_SIZE
+	li $t2, 40
+	sh $t2, ($t1)
+	addiu $t1, $t1, 2
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	#WIDTH
+	sh $ra, ($t1)
+	addiu $t1, $t1, 2
+	srl $t2, $ra, 16
+	sh $t2, ($t1)
+	addiu $t1, $t1, 2
+	#HEIGHT
+	sh $ra, ($t1)
+	addiu $t1, $t1, 2
+	srl $t2, $ra, 16
+	sh $t2, ($t1)
+	addiu $t1, $t1, 2
+	#PLANES
+	li $t2, 1
+	sh $t2, ($t1)
+	addiu $t1, $t1, 2
+	#BPP
+	li $t2, 24
+	sh $t2, ($t1)
+	addiu $t1, $t1, 2
+	#compression
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	#image_size
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	#res X in ppi
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	#res Y in ppi
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	#number of colors in palette
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	#number of important colors
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	sh $zero, ($t1)
+	addiu $t1, $t1, 2
+	
+	move $fp, $t9
+	
+get_data: #unused - for cleaner code
 	li $v0, 4           
 	la $a0, real_q
 	# ask for real part
@@ -28,25 +143,65 @@ main:
 	syscall
 	move $s7, $v0 
 	#save imaginary
-	li $t1, -10240 #load X pos
-	li $t2, -10240 #load Y pos
+	li $t1, 10240
+	divu $s0, $t1, $ra #calc step
+	mul $t1, $ra, -1  #load X pos
+	move $t2, $t1 #load Y pos
+	subi $t1, $t1, 1 #correction for first step
+	#easier than making redundant code for initialisation proceed-julia 
 	la $t3, image #image buffer
-	li $s1, 12 #color const-s
-	li $s2, 55
-	li $s3, 15
+	li $s1, 5 #color const-R
+	li $s2, 1 #color const-G
+	li $s3, 3 #color const-B
+	
+create: #unused - for cleaner code
+	li $v0, 4           
+	la $a0, msg_1
+	syscall
+	
+	la $a0, output
+	li $a1, 1
+	li $a2, 0
+	li $v0, 13
+	syscall
+	
+	move $a0, $v0
+	la $a1, header_bmp
+	li $a2, 54
+	li $v0, 15
+	syscall
+	
+	li $t9, 0	
+	li $s4, BUFF
+	subiu $s4, $s4, 3 # make place for possible zeros
 proceed:
-	li $t0, 0
+	
 step_x:
-	addi $t1, $t1, 40
-	beq $t1, 10240, step_y
+	addi $t1, $t1, 2
+	bge $t1, $ra, step_y
 	j prepare	
 step_y:
-	li $t1, -10240
-	addi $t2, $t2, 40
-	beq $t2, 10240, save_img
-prepare:
-	move $t5, $t1
-	move $t6, $t2
+	move $s5, $fp
+fill_space_zero:
+	beqz $s5, filled
+	sb $zero, ($t3)
+	addiu $t3, $t3, 1
+	addiu $t9, $t9, 1
+	subiu $s5, $s5, 1
+	j fill_space_zero
+	bge $t9, $s4, save
+filled:
+	mul $t1, $ra, -1
+	addi $t2, $t2, 2
+	bge $t2, $ra, save
+	
+prepare: #unused - for cleaner code
+	li $t0, 0
+	bge $t9, $s4, save
+	addiu $t9, $t9, 3
+	
+	mul $t5, $s0, $t1
+	mul $t6, $s0, $t2
 julia:
 	mult $t5,$t5
 	mflo $t7
@@ -84,10 +239,12 @@ julia:
 	addiu $t0, $t0, 1
 	#add iteration
 	blt $t0, 255, julia
-save_j:	
+save_j:	#nesscesary see lines 254-259
+
 	#red
 	mult $t0, $s1
 	mflo $t4
+	li $t4, 255
 	sb $t4, ($t3)
 	addiu $t3, $t3, 1
 	#green
@@ -101,47 +258,23 @@ save_j:
 	sb $t4, ($t3)
 	addiu $t3, $t3, 1	
 	j proceed
-save_img:
-
-	# open file 'input.bmp'
-	la $a0, input
-	li $a1, 0
-	li $a2, 0
-	li $v0, 13
-	syscall
-	
-	move $a0, $v0
-	li $v0, 14 		# read bmp header from file
-	la $a1, header_bmp
-	li $a2, 54
-	syscall
-	
-	# close file 
-	li $v0, 16
-	syscall
-	
-	la $a0, output
-	li $a1, 1
-	li $a2, 0
-	li $v0, 13
-	syscall
-	
-	move $a0, $v0
-	la $a1, header_bmp
-	li $a2, 54
-	li $v0, 15
-	syscall
+save:	
 	
 	la $a1, image
-	li $a2, 786432
+	move $a2, $t9
 	li $v0, 15
 	syscall
-	
+	li $t9, 0
+	la $t3, image
+	bne $t2, $ra, proceed
+close: #unused - for cleaner code	
 	# CLOSING FILE
 	li $v0, 16
 	syscall
-
-end:
+	li $v0, 4           
+	la $a0, msg_2
+	syscall
+end: #unused - for cleaner code
 	li $v0, 10
 	# terminate program
 	syscall	
